05bbdef7e57768fa58a6b7011fa81941c379ce17
renweijie@chinasoftinc.com <renweijie@chinasoftinc.com>
2015-11-01 00:11:48 +0800

    登录相关动画问题修复

--- orange/src/com/orange/m/page/auth/LoginActivity.java
+++ orange/src/com/orange/m/page/auth/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.orange.m.page.auth;
 
 import android.os.Bundle;
-import com.ssn.framework.uikit.BaseActivity;
+import com.orange.m.R;
 import com.ssn.framework.uikit.BaseNavActivity;
 
 /**
@@ -11,5 +11,10 @@ public class LoginActivity extends BaseNavActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.base_stay_orig, R.anim.activity_push_bottom_out);
     }
 }
